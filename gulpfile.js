3cb7759a31524bc31115bd1acaa962b7936e79fd
MatildaDobos <matilda.dobos@gmail.com>
2018-02-25 22:49:30 +0200

    Configured gulp and npm packages for sass

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var del = require('del');
+
+
+gulp.task('clean', function() {
+  return del('./app/css/**/*');
+})
+
+gulp.task('sass', ['clean'], function(){
+  return gulp.src('./app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
+
+gulp.task('default', ['browserSync', 'sass']);
